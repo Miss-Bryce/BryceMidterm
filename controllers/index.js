f088259998a83aa1e23bdf7a12b76278e7d80b80
J. Bryce <53979775+Miss-Bryce@users.noreply.github.com>
2021-10-20 12:00:55 -0400

    Finished Setup, now creating data

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Golfing Midterm!' });
+});
+
+router.get('/teetimes', function(req, res, next) {
+  res.render('teetimes', { title: 'Teetimes'});
 });
 
 module.exports = router;
